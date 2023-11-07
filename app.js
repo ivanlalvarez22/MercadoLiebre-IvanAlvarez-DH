b53b4cbceb6233debef6bce3444d4ad900151481
Ivan L. Alvarez <ivanlalvarez.22@gmail.com>
2023-11-07 17:22:15 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.get('/registro', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/registro.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+const port = process.env.PORT || 3030;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
